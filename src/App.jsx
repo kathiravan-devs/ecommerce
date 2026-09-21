21e08bf7f540cfc2a29b5a53857e374e502e734b
Kathir-bca <kathirg.ca@gmail.com>
2026-09-22 00:24:36 +0530

    add checkout page using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
+import { Checkout } from './pages/Checkout';
 import './App.css';
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>Test checkout</div>} />
-    </Routes>     
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Routes>
     </>
   )
 }
